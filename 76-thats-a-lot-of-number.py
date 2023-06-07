09ef8d3e35ad4da95ce11ca246a704b6916a3156
RubberDuckCollector <119439430+RubberDuckCollector@users.noreply.github.com>
2023-06-07 22:10:08 +0100

    Update 76-thats-a-lot-of-number.py

--- 76-thats-a-lot-of-number.py
+++ 76-thats-a-lot-of-number.py
@@ -1,4 +1,4 @@
-with open("/Users/alfielong/programming/ocr-challenges/76-thats-a-lot-of-number/nums.txt", "r") as f:
+with open("76-thats-a-lot-of-number.txt", "r") as f:
     nums = [int(i) for i in f]
 
 nums = sum(nums)
