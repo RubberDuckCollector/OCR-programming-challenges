d41e214647b9e255d2959902cf29cf0ad34aeffb
RubberDuckCollector <119439430+RubberDuckCollector@users.noreply.github.com>
2023-06-13 10:29:41 +0100

    Create 34-whats-the-day.py

---
+++ 34-whats-the-day.py
@@ -0,0 +1,13 @@
+from datetime import date
+from datetime import datetime as dt
+
+def today():
+    time = dt.now().strftime("%H:%M:%S")
+    print(f"Today is {date.today().strftime('%A %d %B %Y')} at {time}") # type: ignore
+
+date = input("Enter a date (yyyy-mm-dd): ")
+date = dt.strptime(date, "%Y-%m-%d")
+print(date)
+
+
+# today()
